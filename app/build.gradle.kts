c28b04238cb1e81ce121b1066c3181803a01ec14
Abe Pazos <abe@hamoid.com>
2022-12-27 19:01:33 +0100

    Update Processing core, add dependencies examples

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,9 +10,39 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.micycle1:processing-core-4:4.0.1")
+    implementation("com.github.micycle1:processing-core-4:4.1.1")
+    // Add dependencies from https://mvnrepository.com/
+///    implementation("com.google.code.gson:gson:2.10")
+
+    // Add dependencies from `../lib/` folder
+//    implementation(
+//        files(
+//            "../lib/oscP5.jar",
+//            "../lib/toxiclibscore.jar"
+//        )
+//    )
 }
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+        vendor.set(JvmVendorSpec.ADOPTIUM)
+    }
+}
+
 
 application {
     mainClass.set("demo.App")
 }
+
+// example of copying a `config` folder when building
+//distributions {
+//    main {
+//        contents {
+//            into("config") {
+//                from("config")
+//            }
+//        }
+//  }
+//}
+
